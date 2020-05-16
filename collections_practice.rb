4d10eeb4ac2b5d967483f7a18f5b5efade4a5322
Jack Quinoa <jquinoa208@gmail.com>
2020-05-16 03:47:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,27 @@
+def sort_array_asc(integers)
+  integers.sort
+end
+
+def sort_array_desc(integers)
+  integers.sort do |a , b|
+    b <=> a 
+  end
+end
+
+def sort_array_char_count(strings)
+  strings.sort do |a , b|
+    a.length <=> b.length 
+  end
+end
+
+def swap_elements(elements)
+  elements.sort do |a , b|
+  a[1], b[2] = b[2], a[1]
+  end
+end
+
+#def reverse_array(integers)
+ # integers.sort |a , b|
+  #
+#end#
 
